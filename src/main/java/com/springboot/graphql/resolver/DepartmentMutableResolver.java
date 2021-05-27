7ae83b5105d824f6613df14cc7da6b8cdb453485
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-05-27 13:09:06 +0300

    Task 9 : Create mutableresolver and queryresolver for Department, Doctor and Hospital

---
+++ src/main/java/com/springboot/graphql/resolver/DepartmentMutableResolver.java
@@ -0,0 +1,9 @@
+package com.springboot.graphql.resolver;
+
+import graphql.kickstart.tools.GraphQLMutationResolver;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DepartmentMutableResolver implements GraphQLMutationResolver {
+
+}
---
+++ src/main/java/com/springboot/graphql/resolver/DepartmentQueryResolver.java
@@ -0,0 +1,9 @@
+package com.springboot.graphql.resolver;
+
+import graphql.kickstart.tools.GraphQLQueryResolver;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DepartmentQueryResolver implements GraphQLQueryResolver {
+
+}
