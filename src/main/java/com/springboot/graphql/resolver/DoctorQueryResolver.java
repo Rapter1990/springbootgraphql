a7d8cdb25c393efca193c8fd4dd622dfe8c08b2f
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-05-29 22:05:05 +0300

    Task 33 : Add specification features in doctors and doctorById in DoctorQueryResolver

--- src/main/java/com/springboot/graphql/resolver/DoctorQueryResolver.java
+++ src/main/java/com/springboot/graphql/resolver/DoctorQueryResolver.java
@@ -2,12 +2,23 @@ package com.springboot.graphql.resolver;
 
 import com.springboot.graphql.filter.DoctorFilter;
 import com.springboot.graphql.filter.FilterField;
+import com.springboot.graphql.model.Department;
 import com.springboot.graphql.model.Doctor;
+import com.springboot.graphql.model.Hospital;
 import com.springboot.graphql.repository.DoctorRepository;
 import graphql.kickstart.tools.GraphQLQueryResolver;
+import graphql.schema.DataFetchingEnvironment;
+import graphql.schema.DataFetchingFieldSelectionSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
+
+import javax.persistence.criteria.Fetch;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Component
 public class DoctorQueryResolver implements GraphQLQueryResolver {
@@ -19,13 +30,32 @@ public class DoctorQueryResolver implements GraphQLQueryResolver {
         this.doctorRepository = doctorRepository;
     }
 
-    public Iterable<Doctor> doctors() {
-        return doctorRepository.findAll();
+    public Iterable<Doctor> doctors(DataFetchingEnvironment environment) {
+
+        DataFetchingFieldSelectionSet s = environment.getSelectionSet();
+        List<Specification<Department>> specifications = new ArrayList<>();
+        if (s.contains("department") && !s.contains("hospital"))
+            return doctorRepository.findAll(fetchDepartment());
+        else if (!s.contains("department") && s.contains("hospital"))
+            return doctorRepository.findAll(fetchHospital());
+        else if (s.contains("department") && s.contains("hospital"))
+            return doctorRepository.findAll(fetchDepartment().and(fetchHospital()));
+        else
+            return doctorRepository.findAll();
     }
 
-    public Doctor doctor(Integer id) {
-        return doctorRepository.findById(id).get();
+    public Doctor doctor(Integer id, DataFetchingEnvironment environment) {
+
+        Specification<Doctor> spec = byId(id);
+        DataFetchingFieldSelectionSet selectionSet = environment.getSelectionSet();
+        if (selectionSet.contains("department"))
+            spec = spec.and(fetchDepartment());
+        if (selectionSet.contains("hospital"))
+            spec = spec.and(fetchHospital());
+        return doctorRepository.findOne(spec).orElseThrow(NoSuchElementException::new);
+
     }
+
 
     public Iterable<Doctor> doctorsWithFilter(DoctorFilter filter) {
         Specification<Doctor> spec = null;
@@ -40,6 +70,26 @@ public class DoctorQueryResolver implements GraphQLQueryResolver {
             return doctorRepository.findAll(spec);
         else
             return doctorRepository.findAll();
+    }
+
+    private Specification<Doctor> fetchHospital() {
+        return (Specification<Doctor>) (root, query, builder) -> {
+            Fetch<Doctor, Hospital> f = root.fetch("hospital", JoinType.LEFT);
+            Join<Doctor, Hospital> join = (Join<Doctor, Hospital>) f;
+            return join.getOn();
+        };
+    }
+
+    private Specification<Doctor> fetchDepartment() {
+        return (Specification<Doctor>) (root, query, builder) -> {
+            Fetch<Doctor, Department> f = root.fetch("department", JoinType.LEFT);
+            Join<Doctor, Department> join = (Join<Doctor, Department>) f;
+            return join.getOn();
+        };
+    }
+
+    private Specification<Doctor> byId(Integer id) {
+        return (Specification<Doctor>) (root, query, builder) -> builder.equal(root.get("id"), id);
     }
 
     private Specification<Doctor> bySalary(FilterField filterField) {
