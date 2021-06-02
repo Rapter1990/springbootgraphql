cef56fbfff2347a2861b09fd85ba50f80a93b563
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-06-02 11:23:01 +0300

    Task 64 : Change Doctor's position in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,6 +16,6 @@ insert into doctor (id, first_name, last_name, position, salary, age, birthday, 
 insert into doctor (id, first_name, last_name, position, salary, age, birthday, department_id, hospital_id) values (2, 'Adam', 'Hamilton', 'Surgeon', 12000, 38,'1983-03-03', 1, 1);
 insert into doctor (id, first_name, last_name, position, salary, age, birthday, department_id, hospital_id) values (3, 'Tracy', 'Smith', 'Cardiologist', 15000, 36, '1985-02-02',1, 1);
 insert into doctor (id, first_name, last_name, position, salary, age, birthday, department_id, hospital_id) values (4, 'Lucy', 'Kim', 'Surgeon', 13000, 31, '1990-02-01' , 2,1);
-insert into doctor (id, first_name, last_name, position, salary, age, birthday, department_id, hospital_id) values (5, 'Peter', 'Wright', 'Gastroenterologist', 50000, 31, '1990-06-02',4, 2);
+insert into doctor (id, first_name, last_name, position, salary, age, birthday, department_id, hospital_id) values (5, 'Peter', 'Wright', 'Neurologist', 50000, 31, '1990-06-02',4, 2);
 insert into doctor (id, first_name, last_name, position, salary, age, birthday, department_id, hospital_id) values (6, 'Alan', 'Murray', 'Surgeon', 20000, 31, '1990-07-02',4, 2);
 insert into doctor (id, first_name, last_name, position, salary, age, birthday, department_id, hospital_id) values (7, 'Pamela', 'Anderson', 'Endocrinologist', 7000, 31, '1990-08-02',4, 2);
